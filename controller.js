a5371518b3bc9b065da62054b03df384ff27a330
maskurniashidi <maskurshidi12@gmail.com>
2021-10-08 17:15:11 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require("./res");
 var connection = require("./koneksi");
 
 exports.index = function (req, res) {
-  response.ok("aplikasi rest apiku berjalan");
+  response.ok("aplikasi rest apiku berjalan", res);
+};
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function (req, res) {
+  connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
 };
