27e182d02c3a7fbca69d72dd0df4b649b979344b
maskurniashidi <maskurshidi12@gmail.com>
2021-10-08 17:25:10 +0700

    berhasil menampilkan data berdasar id

--- controller.js
+++ controller.js
@@ -2,6 +2,7 @@
 
 var response = require("./res");
 var connection = require("./koneksi");
+const conn = require("./koneksi");
 
 exports.index = function (req, res) {
   response.ok("aplikasi rest apiku berjalan", res);
@@ -11,7 +12,19 @@ exports.index = function (req, res) {
 exports.tampilsemuamahasiswa = function (req, res) {
   connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
     if (error) {
-      connection.log(error);
+      console.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
+
+//menampilkan semua data mahasiswa berdasar id
+exports.tampilberdasarkanid = function (req, res) {
+  let id = req.params.id;
+  connection.query("SELECT * FROM mahasiswa WHERE id_mahasiswa = ?", [id], function (error, rows, fields) {
+    if (error) {
+      console.log(error);
     } else {
       response.ok(rows, res);
     }
